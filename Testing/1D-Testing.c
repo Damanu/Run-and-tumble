85ded60baddc8241148e7a1658f4bd4d2f47dac7
Damanu <e.schwarzhans@gmx.at>
2015-03-30 21:50:47 +0200

    Got a lot of errors, worked on the datatype problem --> roundf does not work, dont know why

--- Testing/1D-Testing.c
+++ Testing/1D-Testing.c
@@ -8,10 +8,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "./../Lib/random.h"
-
+#include <math.h>
+#include <string.h>
 //--------------Prototypes-----------------------
-int[] init(int N, float phi);
-
+int * init_lat(int N,float phi);
+int rand_index(int arraylength);
 //----------------Main Program--------------------
 void main() {
 	printf("--------------------\n");
@@ -30,23 +31,41 @@ void main() {
 	for(i=0;i<N;i++) {
 		printf("%d\n",lattice[i]);
 	}
+	init_lat(N,phi);
 }
 //-----------------Functions-----------------------
 
-//--init--
+//--init_lat--
+//initialises the Lattice with cells on it
 //input:int N (number of sites),float phi (particle concentration)
 //output: int array lattice (lattice with particles at timestep 0)
-int init[](N,phi) {
+int * init_lat(int N,float phi) {
 	long int seed = 123456789;
-	int lattice[N];
-	int M = int(N*phi); 			//M (number of Particles) --> if N*phi >= n.5 (with n natrual number) there is an error. This error is negligible for big N
+	int lattice[N];				
+	float M_ =(float)(N)*phi; 				//M (number of Particles) --> if N*phi >= n.5 (with n natrual number) there is an error. This error is negligible for big N
+	int M = roundf(M_);
+	printf("%d*%f= %d",N,phi,M);
 	double interval=1/N;			//separate the space 0-1 into N pieces with length interval
 	int i = 0;
-	double rndnum;			
-	for(i=0;i<M;i++) { 		
-		rndnum = ran3(&seed);		//pick a random number out of the spacing 0-1
-		index = int(rndnum/interval)	//evaluate the index of the lattice, this index is a randomly chosen one
+	double rndnum;
+	int len = strlen(lattice);
+	printf("length of array:  %d",len);			
+	for(i=0;i<M;i++) { 			//loop to find random indizes
+		rand_index(lattice)
 		 //here is a problem, i wont get the right index cause i cut something off... need to fix this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	}
-	
+	printf("chosen indizes: %d",lattice);
+	return lattice
 }
+
+//--rand_index--
+//returns a random index of given array
+int rand_index(int arraylenght) {
+	long int seed = 123456789;
+	double interval=1/arraylength;		//separate the space 0-1 into N pieces with length interval
+	int i = 0;
+	double rndnum;			
+	rndnum = ran3(&seed);			//pick a random number out of the spacing 0-1
+	index = roundf(rndnum/interval);	//evaluate the index of the lattice, this index is a randomly chosen one		
+}
+
